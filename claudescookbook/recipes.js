214d63cfd691bc219bd936235a9e57c2fc0fb5b2
ctn <35670719+TNg018@users.noreply.github.com>
2023-03-17 17:11:25 -0400

    Update ingredient amounts when yield is changed

--- claudescookbook/recipes.js
+++ claudescookbook/recipes.js
@@ -1,24 +1,85 @@
+// allow scroll through recipes
 var prev = $('.prev');
 var next = $('.next');
 var recipes = $('.recipe-item');
 
-var index = 0;
+// set first recipe as active
+$('.recipes li:first').addClass('active');
 
+// show one recipe (specified node) at a time
 function show( node ){
-  node.addClass('active') // show specified node
-      .siblings().removeClass('active'); // hide previously active node
+  node.addClass('active') 
+      // hide previously active node
+      .siblings().removeClass('active'); 
 }
 
 prev.on('click', function(e){
-  e.preventDefault(); // avoid scrolling to top
-  
+  // prevent scrolling to top
+  e.preventDefault(); 
+
   var previousNode = recipes.filter('.active').prev();
   show(previousNode);
 });
 next.on('click', function(e){
-  e.preventDefault(); // prevent scrolling to top
+  // prevent scrolling to top
+  e.preventDefault(); 
   var nextNode = recipes.filter('.active').next();
   show(nextNode);
 });
 
-$('.recipes li:first').addClass('active');
+
+
+// variable for original yield val
+const ogYield = parseInt(document.getElementById('og-yield').value, 10);
+
+// get ingredient value (hidden) 
+var ingredientAmount = document.getElementsByClassName('ingredient-amount');
+
+// get output ingredient span
+var outputAmountList = document.getElementsByClassName('output-amount');
+
+
+// get ingredient values on page load
+window.onload = function() {
+  for (var i = 0; i < outputAmountList.length; i++) {
+    outputAmountList[i].innerHTML = ingredientAmount[i].innerHTML;
+  }
+}
+
+// update yield based on click (step up)
+function increaseYield() {
+  var value = parseInt(document.getElementById('yield').value, 10);
+  value = isNaN(value) ? 1 : value;
+  value++;
+
+  document.getElementById('yield').value = value;
+  // if yield increases and is not equal to original yield, update yield by (currentYield/ogYield * amount)
+  for (var i = 0; i < outputAmountList.length; i++) {
+    if (ingredientAmount[i].innerHTML == "a") {
+      ingredientAmount[i].innerHTML = 1;
+      outputAmountList[i].innerHTML = ingredientAmount[i].innerHTML; 
+    } 
+    outputAmountList[i].innerHTML = value/ogYield * ingredientAmount[i].innerHTML;
+  }
+}
+
+// update yield based on click (step down)
+function decreaseYield() {
+  var value = parseInt(document.getElementById('yield').value, 10);
+  value = isNaN(value) ? 1 : value;
+  // if value less than 1, set to 1 or keep as current value
+  value < 1 ? 1 : value;
+  // only decrease if value is greater than 1 (no zero)
+  value > 1 ? value-- : 1;
+  
+  document.getElementById('yield').value = value;
+  // if yield increases and is not equal to original yield, update yield by (currentYield/ogYield * amount)
+  for (var i = 0; i < outputAmountList.length; i++) {
+    if (ingredientAmount[i].innerHTML == "a") {
+      ingredientAmount[i].innerHTML = 1;
+      outputAmountList[i].innerHTML = ingredientAmount[i].innerHTML; 
+    } 
+    outputAmountList[i].innerHTML = value/ogYield * ingredientAmount[i].innerHTML;
+  }
+}
+
