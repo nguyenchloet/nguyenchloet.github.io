c06625f21e1dce089b39626bfa6d2e2c400306c2
ctn <35670719+TNg018@users.noreply.github.com>
2023-04-18 16:30:09 -0400

    New One-Page Site Added

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-const showLinks = (id) => {
-   var elem = document.getElementById(id);
-   var ulchild = elem.nextElementSibling;
-   //if(!elem) { 
-   // alert("error: not found!");
-   // } else {
-      if (ulchild.style.display == "block") {
-         ulchild.style.display = "none";
-      } else {
-        ulchild.style.display = "block";
-      }
-   //}
+$(document).ready(function(){
+   $("#linkedin").hover(function(){
+     //$(this).css("background-color", "yellow");
+     }, function(){
+     //$(this).css("background-color", "pink");
+   });
+ });
+
+ // code coach page carousel behavior
+
+var slideshowContainers = document.getElementsByClassName("slideshow-container");
+var slideIndex = 0;
+
+for(let s = 0; s < slideshowContainers.length; s++) {
+	var cycle = slideshowContainers[s].dataset.cycle;
+	var slides = slideshowContainers[s].querySelectorAll('.mySlides');
+  let dots = slideshowContainers[s].querySelectorAll('.dot');
+	showSlides(slides, slideIndex, cycle, dots);
 };
 
-button.addEventListener("click", showLinks);
+function showSlides(slides, slideIndex, cycle, dots) {
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    };
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    };
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(function() {
+		showSlides(slides, slideIndex, cycle, dots)
+	}, cycle);
+};
+
+showSlides();
