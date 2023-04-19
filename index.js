46e853768134dc802b3808fb9a9414e908a4948d
ctn <35670719+TNg018@users.noreply.github.com>
2023-04-19 18:17:47 -0400

    Light and dark mode button toggle

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ $(document).ready(function(){
      //$(this).css("background-color", "pink");
    });
  });
-
+ 
  //dark mode toggle
- function darkModeToggle() {
+function darkModeToggle() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+
+  var modeImg = document.getElementById("mode-image");
+  if (modeImg.getAttribute('src') === "./styles/lightmode.svg") {
+    modeImg.setAttribute('src', "./styles/darkmode.svg");
+}
+else {
+    modeImg.setAttribute('src', "./styles/lightmode.svg");
+}
+
+  
 }
 
  // code coach page carousel behavior
@@ -42,3 +52,16 @@ function showSlides(slides, slideIndex, cycle, dots) {
 };
 
 showSlides();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+      $('#return-to-top').fadeIn(200);    // Fade in the arrow
+  } else {
+      $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+  }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+  $('body,html').animate({
+      scrollTop : 0                       // Scroll to top of body
+  }, 500);
+});
